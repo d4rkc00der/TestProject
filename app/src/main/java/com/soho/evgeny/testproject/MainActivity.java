16f65a24789fdea38ceff666f57e84d7be499898
evgeny <shumakov.e@gmail.com>
2016-09-06 12:40:04 +0300

    Logger v2

--- app/src/main/java/com/soho/evgeny/testproject/MainActivity.java
+++ app/src/main/java/com/soho/evgeny/testproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.soho.evgeny.testproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG,"onCreate");
     }
 }
