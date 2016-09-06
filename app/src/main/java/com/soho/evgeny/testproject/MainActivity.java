5f402c1d12cf46b6523956883a85f31a265c8ded
evgeny <shumakov.e@gmail.com>
2016-09-06 12:16:06 +0300

    Logger

--- app/src/main/java/com/soho/evgeny/testproject/MainActivity.java
+++ app/src/main/java/com/soho/evgeny/testproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String LOG_TAG = "MyLog";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
